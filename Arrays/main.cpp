10c3a6ebcda33c036882b719e88a0dff6d334aa4
hawri <hawri@DESKTOP-BMM1386>
2023-03-22 16:22:14 +0300

    "Homework"

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,14 +1,19 @@
 ﻿#include <iostream>
 using namespace std;
 
-void FillRand(int arr[], const int n);
-void Print(int arr[], const int n);
+void FillRand(int arr[], const int n,int minRand=0,int maxRand=100);
+void FillRand(double arr[], const int n);
+void Print(const int arr[], const int n);
+void Print(const double arr[], const int n);
 int Sum(const int arr[], const int n);
 double Avg(const int arr[], const int n);
 int minValueIn(const int arr[], const int n);
 int maxValueIn(const int arr[], const int n);
+void Sort(int arr[], const int n);
 void shiftLeft(int arr[], const int n, int number_of_shifts);
 void shiftRight(int arr[], const int n, int number_of_shifts);
+void UniqueRand(int arr[], const int n);
+void Search(int arr[], const int n);
 
 void main()
 {
@@ -21,31 +26,55 @@ void main()
 	//   n-1;
 
 	//Ввод массива с клавиатуры:
-	//FillRand(arr, n);
+	FillRand(arr, n,0,5);
+	//UniqueRand(arr, n);
 	//Вывод массива на экран:
 	Print(arr, n);
 	cout << "Сумма элементов массива: " << Sum(arr,n) << endl;
 	cout << "Среднее арифметическое элементов массива: " << Avg(arr,n) << endl;
 	cout << "Минимальное значение в массиве: " << minValueIn(arr, n) << endl;
 	cout << "Максимальное значение в массиве: " << maxValueIn(arr,n) << endl;
+	Search(arr, n);
+	Sort(arr,n);
+	Print(arr,n);
 
-	int number_of_shifts;
-	cout << "Введите количество сдвигов влево: "; cin >> number_of_shifts;
-	shiftLeft(arr, n, number_of_shifts);
-	Print(arr, n);
-	cout << "Введите количество сдвигов вправо: "; cin >> number_of_shifts;
-	shiftRight(arr, n, number_of_shifts);
-	Print(arr, n);
+	//int number_of_shifts;
+	//cout << "Введите количество сдвигов влево: "; cin >> number_of_shifts;
+	//shiftLeft(arr, n, number_of_shifts);
+	//Print(arr, n);
+	//cout << "Введите количество сдвигов вправо: "; cin >> number_of_shifts;
+	//shiftRight(arr, n, number_of_shifts);
+	//Print(arr, n);
+
+	const int SIZE = 8;
+	double d_arr[SIZE];
+	FillRand(d_arr, SIZE);
+	Print(d_arr, SIZE);
 }
-void FillRand(int arr[], const int n)
+void FillRand(int arr[], const int n, int minRand, int maxRand)
 {
 	for (int i = 0; i < n; i++)
 	{
 		//cin >> arr[i];
-		arr[i] = rand() % 100;
+		arr[i] = rand() % (maxRand-minRand)+minRand;
+	}
+}void FillRand(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		//cin >> arr[i];
+		arr[i] = rand() % 10000;
+		arr[i] /= 100;
 	}
 }
-void Print(int arr[], const int n)
+void Print(const int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}void Print(const double arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -86,6 +115,25 @@ int maxValueIn(const int arr[], const int n)
 	}
 	return max;
 }
+void Sort(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		//Счетчик 'i' выбирает элемент в который нужно поместить минимальное значение из перебираемых
+		for (int j = i + 1; j < n; j++)
+		{
+			//Счетчтик 'j' перебирает элементы в поисках минимального
+			//arr[i] - выбранный элемент
+			//arr[j] - перебираемый элемент
+			if (arr[j] < arr[i])
+			{
+				arr[i] ^= arr[j];
+				arr[j] ^= arr[i];
+				arr[i] ^= arr[j];
+			}
+		}
+	}
+}
 void shiftLeft(int arr[], const int n, int number_of_shifts)
 {
 	for (int i = 0; i < number_of_shifts; i++)
@@ -108,5 +156,46 @@ void shiftRight(int arr[], const int n, int number_of_shifts)
 			arr[i] = arr[i - 1];
 		}
 		arr[0] = buffer;
+	}
+}
+void UniqueRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 10;
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[i] == arr[j])
+			{
+				i--;
+				break;
+			}
+		}
+	}
+}
+void Search(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		bool met_before = false;  //Предположим,что искомое число не встречалось в массие ранее,
+		//но это нужно проверить:
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[i] == arr[j])
+			{
+				met_before = true;  //Запоминаем то, что число втсречалось ранее
+				break;
+			}
+		}
+		if (met_before)continue;
+		int count = 0; //счетчик повторений
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[i] == arr[j])
+			{
+				count++;
+			}
+		}
+		if (count)cout << "Значение " << arr[i] << " повторяется " << count << " раз" << endl;
 	}
 }
