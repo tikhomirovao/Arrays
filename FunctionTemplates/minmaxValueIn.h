ec769fb41c4a19fc0a4deb6c5c1e550d986c0514
hawri <hawri@DESKTOP-BMM1386>
2023-04-19 13:27:59 +0300

    Homework_templated_separate

---
+++ FunctionTemplates/minmaxValueIn.h
@@ -0,0 +1,8 @@
+#pragma once
+#include"constans.h"
+#include"stdafx.h"
+template<typename T>T minValueIn(const T arr[], const int n);
+template<typename T> T minValueIn(const T arr[ROWS][COLS], const int ROWS, const int COLS);
+
+template<typename T>T maxValueIn(const T arr[], const int n);
+template<typename T> T maxValueIn(const T arr[ROWS][COLS], const int ROWS, const int COLS);
